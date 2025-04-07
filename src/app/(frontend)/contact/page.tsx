bb806779b59f30fd1b9eede6c007a261dd599326
sagarmusabbir <sagarmusabbir@gmail.com>
2025-04-08 02:54:05 +0600

    hh

--- src/app/(frontend)/contact/page.tsx
+++ src/app/(frontend)/contact/page.tsx
@@ -54,23 +54,22 @@
 //   )
 // }
 
-// import { headers as getHeaders } from 'next/headers.js'
+import { headers as getHeaders } from 'next/headers.js'
 
-// import { getPayload } from 'payload'
-// import React from 'react'
-// import { fileURLToPath } from 'url'
+import { getPayload } from 'payload'
+import React from 'react'
 
-// import config from '@/payload.config'
+import config from '@/payload.config'
 
 import MyFormComponent from '@/components/ContactForm'
 import { Bracket } from '@thirdbracket/bracketui'
 import PageHeader from '@/components/PageHeader'
 
 export default async function Contact() {
-  // const headers = await getHeaders()
-  // const payloadConfig = await config
-  // const payload = await getPayload({ config: payloadConfig })
-  // const { user } = await payload.auth({ headers })
+  const headers = await getHeaders()
+  const payloadConfig = await config
+  const payload = await getPayload({ config: payloadConfig })
+  const { user } = await payload.auth({ headers })
 
   return (
     <Bracket>
